20ad070ab3bcb0a70b1b07b58af73bb6b47136d3
root <root@localhost.localdomain>
2020-04-11 12:19:50 +0200

    avancée sur le vote

--- discPass.py
+++ discPass.py
@@ -1,10 +1,14 @@
+import os
+
 from discord.ext import commands
+from dotenv import load_dotenv
 
-from testSql import session, Member, Poste
+from testSql import session, Member, Poste, Candidature, Vote, Start
 
+load_dotenv()
 bot = commands.Bot(command_prefix=';')
 
-TOKEN = 'Njk1MzI3NDUzODI0MTU1Nzg5.XoYkhA.D7FkLNUAD2lZqVSoFyTW6_SI8JY'
+TOKEN = os.getenv("TOKEN")
 
 
 @bot.command(description='Show information about the project')
@@ -49,11 +53,19 @@ async def verify(context: commands.context.Context, *args):
     if q is None:
         message += "You're not an admin..."
     else:
+        liste = []
         for member in session.query(Member).all():
             for arg in args:
                 if arg in member.name:
-                    member.is_verified = True
-                    message += member.name + " has been verified\n"
+                    liste.append(member)
+        if len(liste) == 1:
+            liste[0].is_verified = True
+            message += liste[0].name + " has been verified\n"
+        else:
+            message += "Did you mean \n"
+            for membre in liste:
+                message += membre.name + "\n"
+        session.commit()
     message += "```"
     await context.channel.send(message)
 
@@ -70,7 +82,8 @@ async def admins(context: commands.context.Context, *args):
             for arg in args:
                 if arg in member.name:
                     member.is_admin = True
-                    message += member.name + " has been verified\n"
+                    message += member.name + " is now admin\n"
+        session.commit()
     message += "```"
     await context.channel.send(message)
 
@@ -80,7 +93,7 @@ async def unverify(context: commands.context.Context, *args):
     """allows an admin to unverify a member"""
     message = "```\n"
     q = session.query(Member).filter(Member.discord_id == context.author.id).filter(Member.is_admin == True).first()
-    if q is not None:
+    if q is None:
         message += "You're not an admin..."
     else:
         for member in session.query(Member).all():
@@ -88,13 +101,14 @@ async def unverify(context: commands.context.Context, *args):
                 if arg in member.name:
                     member.is_verified = False
                     message += member.name + " has been unverified\n"
+        session.commit()
     message += "```"
     await context.channel.send(message)
 
 
 @bot.command(description='adds a post')
 async def add_post(context: commands.context.Context, name, rank):
-    """Adds a Post to the list of posts. Usage : ;addPost name rank"""
+    """Adds a Post. Usage : ;addPost name rank"""
     message = "```"
     p = session.query(Member).filter(Member.discord_id == context.author.id).filter(Member.is_admin == True).first()
     q = session.query(Poste).filter(name == Poste.name).first()
@@ -102,19 +116,20 @@ async def add_post(context: commands.context.Context, name, rank):
         message += "You're not an admin..."
     else:
         if q is not None:
-            message += "The post " + Poste.name + " already exists at rank " + Poste.rank
+            message += "The post " + q.name + " already exists at rank " + str(q.rank)
         else:
             post = Poste(name=name, rank=rank)
             session.add(post)
             session.commit()
-            message += "The post " + Poste.name + " was added at rank " + Poste.rank
+            message += "The post " + post.name + " was added at rank " + str(post.rank)
+        session.commit()
     message += "```"
     await context.channel.send(message)
 
 
 @bot.command(description='removes a post')
-async def remove_post(context: commands.context.Context, name, rank):
-    """removes a Post to the list of posts. Usage : ;removePost name rank"""
+async def remove_post(context: commands.context.Context, name):
+    """removes a Post. Usage : ;removePost name"""
     message = "```"
     p = session.query(Member).filter(Member.discord_id == context.author.id).filter(Member.is_admin == True).first()
     q = session.query(Poste).filter(name == Poste.name).first()
@@ -122,12 +137,148 @@ async def remove_post(context: commands.context.Context, name, rank):
         message += "You're not an admin..."
     else:
         if q is None:
-            message += "The post " + Poste.name + " doesn't exists at rank " + Poste.rank
+            message += "The post " + name + " doesn't exists"
         else:
             session.delete(q)
-            session.commit()
-            message += "The post " + Poste.name + " was removed at rank " + Poste.rank
+            message += "The post " + q.name + " was removed"
+        session.commit()
     message += "```"
+    await context.channel.send(message)
+
+
+@bot.command(description='Candidate to a poste')
+async def candidate(context: commands.context.Context, namePost):
+    """Candidate to a post"""
+    message = "```"
+    verified_member = session.query(Member).filter(Member.discord_id == context.author.id).filter(
+        Member.is_verified == True).first()
+    post_wanted = session.query(Poste).filter(namePost == Poste.name).first()
+    if verified_member is None:
+        message += "You're not verified..."
+    else:
+        if post_wanted is None:
+            message += "This post does NOT exist... Did you mean : \n"
+            posts = session.query(Poste).all()
+            if not posts:
+                message = "```No posts created. Add a post first"
+            else:
+                for post in session.query(Poste).all():
+                    message += post.name + "\n"
+        else:
+            candidat = Candidature(poste_id=post_wanted.id, membre_id=verified_member.id)
+            session.add(candidat)
+            message += verified_member.name + " wants the post of " + post_wanted.name
+    session.commit()
+    message += "```"
+    await context.channel.send(message)
+
+
+@bot.command(description='Remove candidature to a poste')
+async def remove_candidature(context: commands.context.Context, namePost):
+    """Removes your candidature to a post"""
+    message = "```"
+    verified_member = session.query(Member).filter(Member.discord_id == context.author.id).filter(
+        Member.is_verified == True).first()
+    post_unwanted = session.query(Poste).filter(namePost == Poste.name).first()
+    if verified_member is None:
+        message += "You're not verified... So you can't candidate... Why are you trying this ?"
+    else:
+        if not post_unwanted:
+            message += "This post does NOT exist..."
+        else:
+            candidature = session.query(Candidature).filter(post_unwanted.id == Candidature.poste_id).first()
+            if not candidature:
+                message += "Vous n'avez pas candidaté au poste de " + namePost
+            else:
+                message += "Vous avez démissionné de la candidature au poste de " + namePost
+                session.delete(candidature)
+    session.commit()
+    message += "```"
+    await context.channel.send(message)
+
+
+@bot.command(description="starts a voting session")
+async def start_session(context: commands.context.Context, namePost):
+    """Usage : ;start_session post"""
+    message = "```"
+    admin = session.query(Member).filter(Member.discord_id == context.author.id).filter(Member.is_admin == True).first()
+    post = session.query(Poste).filter(namePost == Poste.name).first()
+    started = session.query(Start).first()
+    if not admin:
+        message += "You're not an admin..."
+    elif not started.is_started:
+        message += "A session is already started"
+    else:
+        if not post:
+            message += "Please enter a valid post"
+        else:
+            start = session.query(Start).first()
+            start.is_started = True
+            start.poste_id = post.id
+            session.commit()
+    message += '```'
+    await context.channel.send(message)
+
+
+@bot.command(description="votes for someone")
+async def vote(context: commands.context.Context, name):
+    """Votes for someone"""
+    message = "```"
+    start = session.query(Start).first()
+    votant = session.query(Member).filter(Member.discord_id == context.author.id).filter(
+        Member.id not in Vote.member_id).filter(Member.is_verified == True).first()
+    if votant is None:
+        message += "You're not verified. Ask an admin for verification. Or you have already voted. Or no session was started"
+    elif not start.is_started:
+        message += "Ask an admin to start the vote"
+    else:
+        liste = []
+        for member in session.query(Member).all():
+            if name in member.name:
+                liste.append(member)
+        if len(liste) == 1:
+            voted = session.query(Member).filter(Member.name == liste[0].name).first()
+            message += liste[0].name + " a voté pour" + voted.name + "\n"
+            vote = Vote(member_id=liste[0].id, candidat_id=voted.id)
+        else:
+            message += "Did you mean \n"
+            for membre in liste:
+                message += membre.name + "\n"
+        session.commit()
+    message += " Vous ne pourrez pas changer ! \n```"
+    await context.channel.send(message)
+
+
+@bot.command(description="ends the voting session")
+async def end_session(context: commands.context.Context):
+    """ends session"""
+    message = "```"
+    admin = session.query(Member).filter(Member.discord_id == context.author.id).filter(Member.is_admin).first()
+    start = session.query(Start).first()
+    post = session.query(Poste).filter(start.poste_id == Poste.id).first()
+    started = session.query(Start).first()
+    candidats = session.query(Candidature).filter(Candidature.poste_id == start.poste_id).all()
+    votes = session.query(Vote).filter(Vote.poste_id == start.poste_id).all()
+    m = 0
+    elu = ""
+    if not admin:
+        message += "You're not an admin..."
+    elif not started.is_started:
+        message += "No session started"
+    else:
+        for candidate in candidats:
+            u = 0
+            for vote in votes:
+                if vote.candidat_id == candidate.id:
+                    u += 1
+            if u > m:
+                qui = session.query(Member).filter(Member.id == candidate.member_id).first()
+                elu = qui.name
+        message += elu + " is elected for the post of " + post.name + "\n"
+        start.is_started = False
+        start.poste_id = -1
+        session.commit()
+    message += '```'
     await context.channel.send(message)
 
 
--- testSql.py
+++ testSql.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, create_engine, Boolean
+from sqlalchemy import Column, Integer, String, create_engine, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+
 
 Base = declarative_base()
 
@@ -8,7 +9,7 @@ Base = declarative_base()
 class Member(Base):
     __tablename__ = 'member'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, nullable=False)
     discord_id = Column(Integer)
     is_verified = Column(Boolean)
     is_admin = Column(Integer)
@@ -18,22 +19,44 @@ class Member(Base):
 class Poste(Base):
     __tablename__ = 'poste'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, nullable=False)
     rank = Column(Integer)
 
 
 class Candidature(Base):
     __tablename__ = 'candidature'
     id = Column(Integer, primary_key=True)
-    poste_id = relationship(Poste)
-    membre_id = relationship(Member)
+    poste = relationship('Poste')
+    member = relationship('Member')
+    poste_id = Column(Integer, ForeignKey('poste.id', ondelete="CASCADE"))
+    membre_id = Column(Integer, ForeignKey('member.id', ondelete="CASCADE"))
+
+
+class Vote(Base):
+    __tablename__ = 'vote'
+    id = Column(Integer, primary_key=True)
+    member = relationship('Member')
+    poste = relationship('Poste')
+    candidat = relationship('Candidature')
+    poste_id = Column(Integer, ForeignKey('poste.id', ondelete="CASCADE"))
+    member_id = Column(Integer, ForeignKey("member.id", ondelete="CASCADE"))
+    candidat_id = Column(Integer, ForeignKey('candidat.id', ondelete="CASCADE"))
+
+
+class Start(Base):
+    __tablename__ = 'start'
+    id = Column(Integer, primary_key=True)
+    is_started = Column(Boolean)
+    poste=relationship("Poste")
+    poste_id=Column(Integer, ForeignKey("poste.id", ondelete="CASCADE"))
 
 
 engine = create_engine('sqlite:///sqlDiscord.db')
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
-
-prez = Poste(name="Président", rank=0)
-prez.add(Poste)
-prez.commit()
+plein = session.query(Start).first()
+if not plein:
+    started = Start(is_started=False, poste_id=-1)
+    session.add(started)
+    session.commit()
